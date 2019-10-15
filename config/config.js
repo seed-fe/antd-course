8fd08550f99d06987c1049398cc277eb6488c3bd
Bill Zhang <zzlfullstack@163.com>
2019-10-15 17:29:21 +0800

    实现了基本布局和侧边导航

--- config/config.js
+++ config/config.js
@@ -7,6 +7,12 @@ export default {
     ],
     routes: [{
         path: '/',
-        component: './HelloWorld',
-    }],
+        component: '../layout',
+        routes: [
+          {
+            path: 'helloworld',
+            component: './HelloWorld'
+          },
+        ]
+      }],
 }
