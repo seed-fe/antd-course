b2fae8a671081e19e3ec673a7eb243ee5157f4e6
Bill Zhang <zzlfullstack@163.com>
2020-01-01 21:10:54 +0800

    引入了dva，增加了添加卡片的按钮

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card ,Button } from 'antd';
+import { connect } from 'dva';
 
+const namespace = 'puzzlecards';
+
+const mapStateToProps = (state) => {
+  const cardList = state[namespace].data;
+  return {
+    cardList,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickAdd: (newCard) => {
+      const action = {
+        type: `${namespace}/addNewCard`,
+        payload: newCard,
+      };
+      dispatch(action);
+    },
+  };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class PuzzleCardsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: 'Did you hear about the two silk worms in a race?',
-          punchline: 'It ended in a tie',
-        },
-        {
-          id: 2,
-          setup: 'What happens to a frog\'s car when it breaks down?',
-          punchline: 'It gets toad away',
-        },
-      ],
-    }
-  }
   render() {
     return (
       <div>
         {
-          this.state.cardList.map(card => {
+          this.props.cardList.map(card => {
             return (
               <Card key={card.id}>
                 <div>Q: {card.setup}</div>
@@ -34,6 +40,12 @@ export default class PuzzleCardsPage extends Component {
             );
           })
         }
+        <div>
+          <Button onClick={() => this.props.onClickAdd({
+            setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            punchline: 'here we use dva',
+          })}> 添加卡片 </Button>
+        </div>
       </div>
     );
   }
